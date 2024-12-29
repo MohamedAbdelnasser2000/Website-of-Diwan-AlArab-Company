61c06c8b656018f9890b43601d54ea49dbe1d04f
MohamedAbdelnasser2000 <99351983+MohamedAbdelnasser2000@users.noreply.github.com>
2024-12-29 02:00:14 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GovEgy_2.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GovEgy_2.Startup))]
+namespace GovEgy_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
